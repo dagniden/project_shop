2f63bccae78176022136a8f2c6d2419e1aece295
Daniil Gnidenko <dagnidenko@gmail.com>
2025-09-24 23:01:36 +0300

    Добавлен вывод и значений и имен атрибутов объекта в MixinLog

--- src/mixinlog.py
+++ src/mixinlog.py
@@ -4,5 +4,4 @@ class MixinLog:
         print(repr(self))
 
     def __repr__(self) -> str:
-        values = ", ".join(map(str, self.__dict__.values()))
-        return f"{self.__class__.__name__}({values})"
+        return f"{self.__class__.__name__}({self.__dict__})"
