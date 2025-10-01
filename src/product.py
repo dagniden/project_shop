867eef9660269a230500df5c7a348b2558e08ea3
Daniil Gnidenko <dagnidenko@gmail.com>
2025-10-01 14:03:47 +0300

    Доработка класса product по задаче

--- src/product.py
+++ src/product.py
@@ -11,7 +11,11 @@ class Product(BaseProduct, MixinLog):
         self.name = name
         self.description = description
         self.__price = price
-        self.quantity = quantity
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
+        else:
+            self.quantity = quantity
+
         super().__init__()
 
     def __str__(self) -> str:
